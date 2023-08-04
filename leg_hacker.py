737c800940f6e717e5a8c6f953f96ffea97e88d7
BrailyRS <brailyrs03@gmail.com>
2023-08-04 01:12:16 -0400

    Avance un poco en el programa hice una pequeña validacion para que cuando la variable iteradora (i) sea igual a la variable iteradora (rec_dict, este representa el key del el diccionario leet) siga corriendo el primer bucle

--- leg_hacker.py
+++ leg_hacker.py
@@ -1,3 +1,5 @@
+
+
 print("""
   +===============================================================================+
   | Escribe un programa que reciba un texto y transforme lenguaje natural a       |
@@ -8,3 +10,57 @@ print("""
   |   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a") |
   +===============================================================================+
       """)
+
+def usuarioTexto():
+  usuario : str= input("Ingrese un texto --> ")
+  return usuario
+
+leet : dict = {
+      ' ': ' ',
+    'a': '4',
+    'b': 'I3',
+    'c': '[',
+    'd': ')',
+    'e': '3',
+    'f': '|=',
+    'g': '&',
+    'h': '#',
+    'i': '1',
+    'j': ',_|',
+    'k': '>|',
+    'l': '1',
+    'm': '/\\/\\',
+    'n': '^/',
+    'o': '0',
+    'p': '|*',
+    'q': '(_,)',
+    'r': 'I2',
+    's': '5',
+    't': '7',
+    'u': '(_)',
+    'v': '\\/',
+    'w': '\\/\\/',
+    'x': '><',
+    'y': 'j',
+    'z': '2',
+    '1': 'L',
+    '2': 'R',
+    '3': 'E',
+    '4': 'A',
+    '5': 'S',
+    '6': 'b',
+    '7': 'T',
+    '8': 'B',
+    '9': 'g',
+    '0': 'o'}
+
+text_usuaio = usuarioTexto()
+
+Lenguaje_hacker : list = []
+
+for i in text_usuaio:
+  Lenguaje_hacker.append(i)
+  for rec_dict in leet:
+    if i == rec_dict:
+      print()
+      break
